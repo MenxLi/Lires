bef6cf31afe45ca312007b9b03ff60e6d1d62fda
Li_Mengxun <mengxunli@whu.edu.cn>
2022-02-27 20:11:55 +0800

    auto detect hostname

--- RBMWeb/frontend/assets/serverAddrQuery.js
+++ RBMWeb/frontend/assets/serverAddrQuery.js
@@ -1,17 +1,20 @@
 
 function onBtnClicked(){
-    const btn = document.querySelector("#btn_server_config");
+    // const btn = document.querySelector("#btn_server_config");
     const addrInput = document.querySelector("#server_addr");
     const portInput = document.querySelector("#server_port");
-    const addr = addrInput.value;
+    let addr = addrInput.value;
     let port = portInput.value;
     if (port === ""){
         port = "8079";
     }
+    if (addr === ""){
+        addr = window.location.hostname;
+    }
     localStorage.setItem("RBMServerAddr", addr)
     localStorage.setItem("RBMServerPort", port)
-    const SERVER_ADDR = localStorage.getItem("RBMServerAddr")
-    const SERVER_PORT = localStorage.getItem("RBMServerPort")
+    // const SERVER_ADDR = localStorage.getItem("RBMServerAddr")
+    // const SERVER_PORT = localStorage.getItem("RBMServerPort")
     //console.log(SERVER_ADDR);
     //console.log(SERVER_PORT);
     window.location.href = "main.html"
