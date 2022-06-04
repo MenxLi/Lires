40daf173dbd6294f876d96e49bbb41dd08842f5d
monsoon <mengxunli@whu.edu.cn>
2022-06-04 17:23:39 +0800

    fix: PyQt6 QMessageBox api update

--- resbibman/GUIs/widgets.py
+++ resbibman/GUIs/widgets.py
@@ -22,30 +22,30 @@ class WidgetBase:
 
     def warnDialog(self, messege, info_msg = ""):
         msg_box = QMessageBox()
-        msg_box.setIcon(QMessageBox.Warning)
+        msg_box.setIcon(QMessageBox.Icon.Warning)
         msg_box.setText(messege)
         msg_box.setInformativeText(info_msg)
         msg_box.setWindowTitle("Warning")
-        msg_box.setStandardButtons(QMessageBox.Ok)
+        msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
         return msg_box.exec()
     
     def warnDialogCritical(self, messege, info_msg = "Please restart the program"):
         msg_box = QMessageBox()
-        msg_box.setIcon(QMessageBox.Critical)
+        msg_box.setIcon(QMessageBox.Icon.Critical)
         msg_box.setText(messege)
         msg_box.setInformativeText(info_msg)
         msg_box.setWindowTitle("Critical warning")
-        msg_box.setStandardButtons(QMessageBox.Ok)
+        msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
         return msg_box.exec()
     
     def queryDialog(self, msg, title = "Query", func = lambda x: None) -> bool:
         msg_box = QMessageBox()
         msg_box.setText(msg)
         msg_box.setWindowTitle(title)
-        msg_box.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg_box.setStandardButtons(QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel)
         msg_box.buttonClicked.connect(func)
         return_value = msg_box.exec()
-        if return_value == QMessageBox.Ok:
+        if return_value == QMessageBox.StandardButton.Ok:
             return True
         else: return False
 
