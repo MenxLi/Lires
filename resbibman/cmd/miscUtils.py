8f604ad381f4bd980fb5965b558d613593bc33dc
limengxun <limengxun45@outlook.com>
2022-12-31 16:06:34 +0800

    feat: rbm-utils.edit_config

--- resbibman/cmd/miscUtils.py
+++ resbibman/cmd/miscUtils.py
@@ -9,11 +9,23 @@ def main():
     parser_download_pdfjs = sub_parser.add_parser("download_pdfjs", help="download PDF.js distribution")
     parser_download_pdfjs.add_argument("-u", "--url", help = "downloading url", default=DEFAULT_PDFJS_DOWNLOADING_URL)
 
+    parser_edit_config = sub_parser.add_parser("edit_config", help="edit configuration file")
+    parser_edit_config.add_argument("-u", "--use_editor", default="", help="choose editor, e.g. . -u vim will invoke 'vim <config>.json'")
+
     args = parser.parse_args()
 
     if args.subparser == "download_pdfjs":
         from resbibman.core.pdfTools import downloadDefaultPDFjsViewer
         downloadDefaultPDFjsViewer(download_url=args.url)
+    if args.subparser == "edit_config":
+        from resbibman.confReader import CONF_FILE_PATH
+        from resbibman.core.utils import openFile
+        import subprocess
+        if args.use_editor:
+            subprocess.call([args.use_editor, CONF_FILE_PATH])
+        else:
+            openFile(CONF_FILE_PATH)
+
 
 if __name__ == "__main__":
     main()
