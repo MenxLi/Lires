fcd467164ec71079f7a3afed68c089863958aa2e
Li, Mengxun <mengxunli@whu.edu.cn>
2023-03-01 15:34:57 +0800

    feat: custom error

---
+++ resbibman/core/customError.py
@@ -0,0 +1,13 @@
+
+class RBMError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class RBMToBeImplementedError(RBMError):
+    def __init__(self, err_msg=None):
+        if err_msg is None:
+            err_msg = "This feature is not yet implemented."
+        super().__init__(err_msg)
+
+class RBMDocTypeNotSupportedError(RBMToBeImplementedError):
+    pass
