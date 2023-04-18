d467c2d7a3c67322a3fd8bc7a7be2b705528bccd
limengxun <limengxun45@outlook.com>
2023-04-19 00:14:28 +0800

    feat(rbmweb): sort file selector by date added

--- RBMWeb2/src/components/FileSelector.vue
+++ RBMWeb2/src/components/FileSelector.vue
@@ -2,17 +2,29 @@
 
 
 <script setup lang="ts">
+    import { computed } from 'vue';
     import FileRow from './FileRow.vue';
     import type { DataBase, DataPoint } from '@/core/dataClass';
     const props = defineProps<{
         database: DataBase
         showUids: string[]
     }>()
+
+    const sortedUIDs = computed(() => props.showUids.sort(
+        function(a, b){
+            // criterion
+                const ca = props.database.get(a).info["time_added"];
+                const cb = props.database.get(b).info["time_added"];
+                if (ca === cb) { return 0};
+                if (ca < cb){ return 1 } else{return -1}
+            }
+        ))
+
 </script>
 
 <template>
     <div class="panel">
-        <FileRow v-for="uid in showUids" :datapoint="database.data[uid]"></FileRow>
+        <FileRow v-for="uid in sortedUIDs" :datapoint="database.data[uid]"></FileRow>
     </div>
 </template>
 
