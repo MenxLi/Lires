12f677b7fcaddadd97baf03561dc8fb519c5e479
limengxun <limengxun45@outlook.com>
2023-05-31 15:53:00 +0800

    fix: fs-vicuna-13b

--- _test/test_fs.py
+++ _test/test_fs.py
@@ -1,12 +1,12 @@
 
 import os
-os.environ["FASTCHAT_SERVER"] = "http://localhost:8000/v1"
+os.environ["FASTCHAT_SERVER"] = "http://192.168.124.11:8000/v1"
 from iRBM.lmInterface import streamOutput,getStreamIter
 
 text = ""
 print("generating....")
 
-ai = getStreamIter("fs-vicuna-13b-v1.1")
+ai = getStreamIter("fs-vicuna-13b")
 streamOutput(ai("Write a story about a robot that is trying to take over the world in around 100 words."))
 streamOutput(ai("Summarize the story in 50 words"))
 
