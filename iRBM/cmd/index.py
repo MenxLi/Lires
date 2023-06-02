b3ad4f83503cd629fb8e7407ebf1a19847c6f069
limengxun <limengxun45@outlook.com>
2023-06-02 10:27:36 +0800

    refactor: textfeature.py

--- iRBM/cmd/index.py
+++ iRBM/cmd/index.py
@@ -1,19 +1,13 @@
 """
 Build search index for the database
 """
-import argparse, asyncio, os
-from functools import wraps
+import argparse
 
-import torch, tqdm
-import openai.error
 from resbibman.confReader import DOC_FEATURE_PATH, getConf
-from resbibman.core.dataClass import DataBase, DataPoint
-from resbibman.core.pdfTools import getPDFText
+from resbibman.core.dataClass import DataBase
 
-from ..lmTools import featurize, structuredSummerize
-from ..lmInterface import StreamIterType
+from ..textFeature import buildFeatureStorage, queryFeatureIndex, queryFeatureIndexByUID
 from ..utils import MuteEverything
-from ..utils import Timer
 
 
 def parseArgs() -> argparse.Namespace:
@@ -38,105 +32,6 @@ def parseArgs() -> argparse.Namespace:
         exit()
     return args
 
-FeatureDict = dict[str, torch.Tensor]
-def buildFeatureIndex(
-        db: DataBase, 
-        max_words_per_doc: int, 
-        model_name: StreamIterType, 
-        force = False
-        ):
-    if os.path.exists(DOC_FEATURE_PATH) and not force:
-        feature_dict = torch.load(DOC_FEATURE_PATH)
-    else:
-        feature_dict = {}
-    
-    def createSummaryWithLLM(text: str, dp: DataPoint):
-        summary = ""
-        _summary_title: str = "Title: " + dp.title + "\n"
-        print("\n" + _summary_title, end=" ")
-
-        __trail_count = 0
-        __max_trail = 3
-        while __trail_count < __max_trail and summary == "":
-            try:
-                summary = asyncio.run(structuredSummerize(text, model=model_name, print_func=print))
-                summary = _summary_title + summary
-            except openai.error.APIError as e:
-                if __trail_count == __max_trail - 1:
-                    print("ERROR: MAX TRAIL REACHED, SKIPPING THIS DOC WITH ONLY TITLE")
-                    summary = _summary_title
-                print(e)
-                print("OpenAI API error, retrying...")
-                __trail_count += 1
-        return summary
-    
-    for idx, (uid, dp) in enumerate(tqdm.tqdm(db.items())):
-        if not (dp.is_local and dp.has_file and dp.fm.file_extension == ".pdf"):
-            continue
-        if uid in feature_dict:
-            print(f"Skipping re-build feature index for {dp}")
-            continue
-        assert dp.file_path is not None  # type hint
-
-        # load pdf
-        doc_path = dp.file_path
-        pdf_text = getPDFText(doc_path, max_words_per_doc)
-        
-        if model_name != "":
-            summary = createSummaryWithLLM(pdf_text, dp)
-        else:
-            summary = pdf_text
-            if summary == "":
-                print("Warning: empty pdf text, use title only: {}".format(dp.title))
-                summary = dp.title
-
-        # featurize the summary
-        feature_dict[uid] = asyncio.run(featurize(summary, dim_reduce=True))  # [d_feature]
-    
-        # save the feature dict on every 10 document to avoid losing progress
-        if idx % 10 == 0:
-            torch.save(feature_dict, DOC_FEATURE_PATH)
-    torch.save(feature_dict, DOC_FEATURE_PATH)
-
-def queryFeatureIndex(query: str, n_return: int = 16) -> list[str]:
-    if os.path.exists(DOC_FEATURE_PATH):
-        feature_dict: FeatureDict = torch.load(DOC_FEATURE_PATH)
-    else:
-        raise FileNotFoundError("Feature index not found, please build the index first")
-    query_vec = asyncio.run(featurize(query, dim_reduce=True)) # [d_feature]
-
-    # collect uuid and features in to a large chunk for batch processing
-    uuids = []
-    features = []
-    for uid, vec in feature_dict.items():
-        uuids.append(uid)
-        features.append(vec)
-    features = torch.stack(features)  # [n_docs, d_feature]
-
-    # find the closest doc by cosine similarity (dot product)
-    scores = torch.matmul(features, query_vec)  # [n_docs]
-
-    if n_return > len(scores):
-        n_return = len(scores)
-    # return the top n_return of the corresponding uuids
-    topk = torch.topk(scores, n_return)
-    print("Top scores:", topk.values)
-    return [uuids[i] for i in topk.indices]
-
-def queryRelatedDocuments(db: DataBase, query_uid: str, n_return: int = 16) -> list[str]:
-    """
-    query the related documents of the given uid
-    """
-    # read the document with the given uid
-    pdf_path = db[query_uid].file_path
-    if pdf_path is None:
-        query_string: str = db[query_uid].title
-        print("Warning: no pdf file found, use title only: {}".format(query_string))
-    else:
-        query_string = getPDFText(pdf_path, 4096)
-    return queryFeatureIndex(query_string, n_return)
-
-
 def main():
     args = parseArgs()
     with MuteEverything():
@@ -144,11 +39,11 @@ def main():
         db = DataBase().init(db_path, force_offline=True)
 
     if args.subparser == "build":
-        buildFeatureIndex(db, force=args.force, max_words_per_doc=args.max_words, model_name=args.model)
+        buildFeatureStorage(db, force=args.force, max_words_per_doc=args.max_words, model_name=args.model)
 
     elif args.subparser == "query":
         if args.input_uid:
-            res = queryRelatedDocuments(db, args.aim, args.n_return)
+            res = queryFeatureIndexByUID(db, args.aim, args.n_return)
         else:
             res = queryFeatureIndex(args.aim, args.n_return)
         print("-----------------------------------")
