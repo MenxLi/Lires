075074dc1bd4d351387d9ac37a2c624eb6178d9e
limengxun <limengxun45@outlook.com>
2023-06-11 17:33:48 +0800

    fix: vite build config

--- RBMWeb2/vite.config.ts
+++ RBMWeb2/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    rollupOptions:{
+      input:{
+        main:resolve(__dirname,'index.html'),
+        login:resolve(__dirname,'login.html')
+      }
+    }
+  }
 })
