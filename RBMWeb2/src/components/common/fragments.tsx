9a5363ea54ea842db20d0644c50f252693b5ff75
limengxun <limengxun45@outlook.com>
2023-06-21 10:11:06 +0800

    feat: tsx and file select button

---
+++ RBMWeb2/src/components/common/fragments.tsx
@@ -0,0 +1,43 @@
+import { defineComponent, ref } from "vue";
+
+export const FileSelectButton = defineComponent({
+    name: 'file-select-button',
+    props: {
+        asLink: {
+            type: Boolean,
+            default: false
+        },
+        text: {
+            type: String,
+            default: 'Upload'
+        },
+        action: {
+            // callable that takes a file and returns a promise
+            type: Function,
+            required: true
+        },
+    },
+    setup(props, {}) {
+        const inputButton = ref<HTMLInputElement | null>(null);
+        const handleFile = (e: Event) => {
+            console.log('handleFile');
+            const input = e.target as HTMLInputElement;
+            if (input.files && input.files.length > 0) {
+              const file = input.files[0];
+              props.action(file);
+            }
+          }
+        return () => (
+            <div class="upload-button">
+                <input type="file" id="upload-file" onChange={handleFile}
+                    ref={inputButton} style={{display: 'none'}} />
+                {
+                    props.asLink ?
+                    <a rel="noopener noreferrer" onClick={() => inputButton.value!.click()}>{props.text}</a>
+                    :
+                    <button type="button" class="btn btn-primary" onClick={() => inputButton.value!.click()}>{props.text}</button>
+                }
+            </div>
+        )
+    }
+})
