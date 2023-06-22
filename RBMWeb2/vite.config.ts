91df52f86d937303f6770ac07948288f43240683
limengxun <limengxun45@outlook.com>
2023-06-22 09:57:02 +0800

    fix: resolve chunksize warning

--- RBMWeb2/vite.config.ts
+++ RBMWeb2/vite.config.ts
@@ -10,9 +10,17 @@ export default defineConfig({
     "host": '0.0.0.0'
   },
   build:{
+    chunkSizeWarningLimit: 1200,
     rollupOptions:{
       input:{
         main:resolve(__dirname,'index.html'),
+      },
+      output:{
+        manualChunks(id) {
+            if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+        }
       }
     }
   }
