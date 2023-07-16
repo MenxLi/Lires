9b27615c66ce55154d013121633be78a9ff11768
limengxun <limengxun45@outlook.com>
2023-07-16 18:55:25 +0800

    fix: rbm-share

--- resbibman/cmd/rbmShare.py
+++ resbibman/cmd/rbmShare.py
@@ -7,19 +7,17 @@ import urllib.parse
 from resbibman.confReader import getConf
 
 def main():
-	raise DeprecationWarning("This script needs update, may be implemented in the future.")
 	parser = argparse.ArgumentParser("rbm-share")
 	parser.add_argument("-t", "--tags", default=[], nargs="+")
 	parser.add_argument("-p", "--port", default="8081", help="port of the rbmweb frontend")
 
 	args = parser.parse_args()
-	# addr = "http://{}:{}/frontend/index.html".format(getConf()["host"], getConf()["port"])
-	addr = "http://{}:{}".format(getConf()["host"], args.port)
+	addr = "{}:{}".format(getConf()["host"], args.port)
 	
 	params = {
 		"tags":"&&".join(args.tags)
 	}
-	aim_addr = "{}?{}".format(addr, urllib.parse.urlencode(params)) 
+	aim_addr = "{}/#/?{}".format(addr, urllib.parse.urlencode(params)) 
 	print(aim_addr)
 
 if __name__ == "__main__":
