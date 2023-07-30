3f5b8dd6d2bdfaaf1df1182895cb364901f4b979
Li, mengxun <limengxun45@outlook.com>
2023-07-30 18:09:43 +0800

    feat: cluster feature

--- resbibman/cmd/visFeat3d.py
+++ resbibman/cmd/visFeat3d.py
@@ -7,7 +7,9 @@ import torch
 from typing import TypedDict
 import os
 
-from sklearn.manifold import TSNE
+from sklearn.manifold import TSNE 
+from sklearn.cluster import SpectralClustering
+
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -20,23 +22,40 @@ class FeatDictItemWthTSNE(TypedDict):
     hash: str
     tsne: torch.Tensor
     desc: str
+    cluster_label: int
 
+print("Loading database...")
+db = DataBase(getConf()["database"], force_offline=True)
 feat_dict: dict[str, FeatDictItemWthTSNE] = torch.load(DOC_FEATURE_PATH)
 
-all_feat = torch.stack([feat_dict[uid]["feature"] for uid in feat_dict]).numpy()
+all_feat: np.ndarray = torch.stack([feat_dict[uid]["feature"] for uid in feat_dict]).numpy()
 print("all_feat: ", all_feat.shape)
 
 # tsne = TSNE(n_components=2, random_state=100, init="pca", perplexity=30)
 tsne = TSNE(n_components=3, random_state=100, perplexity=10)
 all_feat_tsne = tsne.fit_transform(all_feat)
-print("all_feat_tsne: ", all_feat_tsne.shape)
+
+N_CLUSTER = len([t for t in db.total_tags if t[0].isalpha()])
+if len(all_feat) < N_CLUSTER:
+    N_CLUSTER = len(all_feat)
+
+print("Clustering...")
+# clustering = KMeans(
+#     n_clusters=N_CLUSTER,
+#     n_init="auto",
+#     ).fit(all_feat)
+clustering = SpectralClustering(
+    n_clusters=N_CLUSTER,
+    n_jobs=-1,
+).fit(all_feat)
+labels = clustering.labels_
+print("Total number of clusters: ", np.unique(labels).shape[0])
 
 for i, uid in enumerate(feat_dict):
     feat_dict[uid]["uid"] = uid
     feat_dict[uid]["tsne"] = all_feat_tsne[i]
+    feat_dict[uid]["cluster_label"] = labels[i]
 
-print("Loading database...")
-db = DataBase(getConf()["database"], force_offline=True)
 
 print("Cleaning data")
 # Remove the feature that is not in the database
@@ -58,14 +77,17 @@ data = pd.DataFrame({
     'short_desc': [db[uid].getAuthorsAbbr() for uid in uids],
     'desc': [feat_dict[uid]["desc"] for uid in uids],
     'tags': [", ".join(db[uid].tags.toOrderedList()) for uid in uids],
+    'cluster_label': [feat_dict[uid]["cluster_label"] for uid in uids],
 })
 
 # Create a 3D scatter plot with Plotly
-# fig = px.scatter_3d(data, x='x', y='y', z='z', hover_data=['uid', 'desc'], text='short_desc')
-fig = px.scatter_3d(data, x='x', y='y', z='z', hover_data=['uid', 'desc'])
+fig = px.scatter_3d(data, x='x', y='y', z='z', hover_data=['uid', 'desc'], color='cluster_label', color_continuous_scale='Viridis')
 fig.update_traces(marker=dict(size=6))
 
 # Save the interactive plot to an HTML file using offline mode
 out_file = os.path.join(TMP_DIR, "hover_glyph_3d.html")
 pyo.plot(fig, filename=out_file, auto_open=False)
 print("Saved to: ", out_file)
+
+# plot_html = fig.to_html(full_html=False)
+# print(plot_html)
