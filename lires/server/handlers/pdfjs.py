acca85c98689026fd1d088da3a86d6ec48f5acb7
limengxun <limengxun45@outlook.com>
2023-12-25 19:07:34 +0800

    chore: pdfjs not exists error msg

--- lires/server/handlers/pdfjs.py
+++ lires/server/handlers/pdfjs.py
@@ -3,11 +3,14 @@ Serve pdfjs files
 """
 
 from ._base import *
+import os
 from lires.confReader import PDF_VIEWER_DIR
 
 class PdfJsHandler(tornado.web.StaticFileHandler, RequestHandlerMixin):
     root_dir = PDF_VIEWER_DIR
-    G.logger_lrs.info("Using pdf viewer: {}".format(root_dir))
+    if not os.path.exists(os.path.join(root_dir, "web/viewer.html")):
+        G.logger_lrs.error("Pdf viewer not found: {}".format(root_dir))
+
     def get(self, *args, **kwargs):
         # to prevent unauthorized access! 
         # otherwise, anyone can access the pdf viewer and this may become a public pdf viewer...?
