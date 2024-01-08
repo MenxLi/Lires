17e384821a3f15bc1fca5f1098937d1d7160c0c1
limengxun <limengxun45@outlook.com>
2024-01-08 13:40:51 +0800

    chore: minor

--- lires/__init__.py
+++ lires/__init__.py
@@ -1,12 +1,5 @@
-import functools
-import requests
-
 from .core import globalVar as G
 from .version import VERSION
 
 G.init()
 __version__ = VERSION
-
-# disable requests https cerificate verification globally
-requests.get = functools.partial(requests.get, verify=False)
-requests.post = functools.partial(requests.post, verify=False)
