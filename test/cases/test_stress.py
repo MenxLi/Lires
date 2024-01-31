1bcc3f776513aa1b61bef08705b92245e0b981d1
limengxun <limengxun45@outlook.com>
2024-01-31 10:53:01 +0800

    test: stress

---
+++ test/cases/test_stress.py
@@ -0,0 +1,29 @@
+from lires.api import ServerConn
+from lires.utils import Timer
+import asyncio
+from .base import BaseConfig
+import pytest
+
+@pytest.fixture(scope="module")
+def serverConn():
+    config = BaseConfig()
+    return ServerConn(config.admin_user["token"], "http://localhost:8080")
+
+N_CONCURRENT = 10
+SEMAPHORE = asyncio.Semaphore(N_CONCURRENT)
+class TestStress:
+    async def test_status(self, serverConn: ServerConn):
+        async def makeRequest(conn: ServerConn) -> bool:
+            try:
+                async with SEMAPHORE:
+                    await conn.status()
+                return True
+            except Exception as e:
+                print(e)
+                return False
+        n_req = 100
+        with Timer(f"{n_req} requests (concurrent {N_CONCURRENT})"):
+            ret = await asyncio.gather(*[makeRequest(serverConn) for _ in range(n_req)])
+        
+        success = ret.count(True)
+        assert success/len(ret) >= 0.9
--- test/stress_test.py
+++ test/stress_test.py
@@ -2,26 +2,29 @@ import sys
 from lires.user import encrypt
 from lires.api import ServerConn
 from lires.utils import Timer
-import asyncio, multiprocessing
+import asyncio
 
+N_CONCURRENT = 10
+SEMAPHORE = asyncio.Semaphore(N_CONCURRENT)
 async def makeRequest(conn: ServerConn) -> bool:
     # when the server do status check, it will check avaliable documents, uptime, number of connections, etc.
     # the server will also check if the token is valid and make some log
     # so this is a good way to stress test the server
     try:
-        await conn.status()
+        async with SEMAPHORE:
+            await conn.status()
         return True
     except Exception as e:
         print(e)
         return False
 
-N_CONCURRENT = 10
-def run(conn):
-    global N_CONCURRENT
-    async def makeConcurrentRequests():
-        # send 10 requests concurrently
-        return await asyncio.gather(*[makeRequest(conn) for _ in range(N_CONCURRENT)])
-    return asyncio.run(makeConcurrentRequests())
+async def main(username: str, password: str):
+    token = encrypt.encryptKey(username, encrypt.generateHexHash(password))
+    conn = ServerConn(token, "http://localhost:8080")
+    n_req = 1000
+    with Timer(f"{n_req} requests (concurrent {N_CONCURRENT})"):
+        res = await asyncio.gather(*[makeRequest(conn) for _ in range(n_req)])
+    print(f"Success: {res.count(True)} / {n_req}")
 
 if __name__ == "__main__":
 
@@ -30,17 +33,4 @@ if __name__ == "__main__":
     except IndexError:
         print("Usage: stress_test.py <username> <password>")
         exit(1)
-
-    token = encrypt.encryptKey(username, encrypt.generateHexHash(password))
-    conn = ServerConn(token, "http://localhost:8080")
-
-    n_req = 100
-    conn_pool = [conn] * n_req
-    with multiprocessing.Pool(4) as pool:
-        with Timer(f"{n_req} * {N_CONCURRENT} requests"):
-            res = pool.map(run, conn_pool)
-    
-    __all_res = []
-    for _res in res:
-        __all_res.extend(_res)
-    print(f"Success: {__all_res.count(True)} / {len(__all_res)}")
+    asyncio.run(main(username, password))
