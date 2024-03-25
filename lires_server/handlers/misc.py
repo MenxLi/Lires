e3a5f9953973b129e688e83121d591d713169703
limengxun <limengxun45@outlook.com>
2024-03-25 18:35:52 +0800

    chore: api main file

--- lires_server/handlers/misc.py
+++ lires_server/handlers/misc.py
@@ -6,6 +6,7 @@ some small handlers that are not worth putting in a separate file
 from ._base import *
 from lires import VERSION
 from lires_server.types import ServerStatus
+import string
 import json, time, os
 
 class ReloadDBHandler(RequestHandlerBase):
@@ -38,6 +39,20 @@ class StatusHandler(RequestHandlerBase):
     async def post(self):
         await self._respond()
 
+_api_mainfile_template = string.Template(f"""
+// Lires-API v$VERSION (Accessed: $TIME at $URL)
+// Run this script with Node.js
+// Set `NODE_TLS_REJECT_UNAUTHORIZED=0` to ignore SSL error
+
+import {{ ServerConn }} from "./api.js";
+
+const context = {{
+    endpoint: "$URL",
+    key: "$KEY"
+}}
+const conn = new ServerConn(()=>context.endpoint, ()=>context.key)
+console.log(await conn.status()) 
+""")
 class APIGetHandler(RequestHandlerBase):
     @keyRequired
     async def get(self):
@@ -46,27 +61,21 @@ class APIGetHandler(RequestHandlerBase):
         api_d_ts = os.path.join(LRSWEB_APIFILE_ROOT, "api", "serverConn.d.ts")
         protocol_d_ts = os.path.join(LRSWEB_APIFILE_ROOT, "api", "protocol.d.ts")
 
-        example_content = f"""
-        // Run this script with Node.js
-        // set NODE_TLS_REJECT_UNAUTHORIZED=0 to ignore SSL error
-
-        import {{ ServerConn }} from "./api.js";
-        const conn = new ServerConn(()=>'$URL', ()=>'$KEY')
-        console.log(await conn.status()) 
-        """.replace("        ", "").replace(
-            "$URL", self.request.protocol + "://" + self.request.host
-        ).replace(
-            "$KEY", (await self.userInfo())['enc_key']
+        example_content = _api_mainfile_template.safe_substitute(
+            TIME=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            VERSION=VERSION,
+            URL=self.request.protocol + "://" + self.request.host,
+            KEY=(await self.userInfo())['enc_key']
         )
-        
+
         # bundle to zip
         import zipfile
         import io
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, "w") as z:
-            z.write(api_file, "api.js")
-            z.write(api_d_ts, "api.d.ts")
-            z.write(protocol_d_ts, "protocol.d.ts")
+            z.write(api_file, "lib/api.js")
+            z.write(api_d_ts, "lib/api.d.ts")
+            z.write(protocol_d_ts, "lib/protocol.d.ts")
             z.writestr("main.mjs", example_content)
         zip_buffer.seek(0)
         self.set_header("Content-Type", "application/zip")
