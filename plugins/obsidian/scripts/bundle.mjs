2005ab1e2965a8b3e213ea8b49ed80eb995a1998
limengxun <limengxun45@outlook.com>
2024-04-16 10:42:56 +0800

    refactor: obsidian plugin to asset

--- plugins/obsidian/scripts/bundle.mjs
+++ plugins/obsidian/scripts/bundle.mjs
@@ -1,4 +1,6 @@
 import fs from 'fs-extra';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
 const neededFiles = [
     'styles.css',
@@ -6,8 +8,22 @@ const neededFiles = [
     'versions.json',
 ];
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const srcRoot = path.resolve(__dirname, '..');
+const distDir = path.resolve(srcRoot, 'dist');
+const projectRoot = path.resolve(srcRoot, '../..');
+const assetDestDir = path.resolve(projectRoot, 'lires_server/assets/obsidian-plugin');
+
 neededFiles.forEach(file => {
-    fs.copy(`./${file}`, `./dist/${file}`)
-        // .then(() => console.log(`${file} copied successfully!`))
+    fs.copy(`./${file}`, `${distDir}/${file}`)
         .catch(err => console.error(err));
 })
+console.info('Copied needed files to dist dir');
+
+// make another copy to asset dir
+neededFiles.forEach(file => {
+    fs.copy(`./${file}`, `${assetDestDir}/${file}`)
+        .catch(err => console.error(err));
+})
+console.info('Copied needed files to asset dir');
