3a425097f9fa3bf2e3eaa00e906e1da9d2607128
limengxun <limengxun45@outlook.com>
2025-04-05 12:02:02 +0800

    fix: file type assurance for pdfjs handler

--- lires_server/handlers/pdfjs.py
+++ lires_server/handlers/pdfjs.py
@@ -11,9 +11,20 @@ asyncio.run(init_pdf_viewer(PDF_VIEWER_DIR))
 class PdfJsHandler(tornado.web.StaticFileHandler, RequestHandlerMixin, print_init_info = False):
     root_dir = PDF_VIEWER_DIR
 
-    async def get(self, *args, **kwargs):
+    def set_extra_headers(self, path: str) -> None:
+        # file type assurance
+        super().set_extra_headers(path)
+
+        if path.endswith(".js") or path.endswith(".mjs"):
+            self.set_header("Content-Type", "application/javascript")
+        if path.endswith(".pdf"):
+            self.set_header("Content-Type", "application/pdf")
+        if path.endswith(".svg"):
+            self.set_header("Content-Type", "image/svg+xml")
+
+    async def get(self, path: str, *args, **kwargs):
         # to prevent unauthorized access! 
         # otherwise, anyone can access the pdf viewer and this may become a public pdf viewer...?
-        if "web/viewer.html" in args:
+        if "web/viewer.html" in path:
             await self.check_key()
-        return await super().get(*args, **kwargs)
+        return await super().get(path, *args, **kwargs)
